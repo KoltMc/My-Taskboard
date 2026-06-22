a8a3a87a25ae542b417f1ffa7e9a9900eb163a74
KoltMc <163803345+KoltMc@users.noreply.github.com>
2026-06-22 01:37:51 -0500

    Add event listeners for register and login buttons

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ document.getElementById("saveBtn").addEventListener("click", async () => {
 
     console.log(text); // this prints in the browser console
 });
+
+document.getElementById("registerBtn").addEventListener("click", async () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log(username);
+    console.log(password);
+
+    const response = await fetch("/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+
+    const result = await response.text();
+    console.log(result);
+});
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log(username);
+    console.log(password);
+
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+})
